bbce313bbc4271a799e32e7957f5dcd74e27a8ea
Joe Kaufeld <joe@kenzie.academy>
2021-07-18 18:46:34 -0400

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="blossom-wrapper",
-    version="0.0.2",
+    version="0.3.0",
     author="Grafeas Group",
     author_email="info@grafeas.org",
     description="Wrapper for the Blossom API",
