ef95ecbcaa607afabc226214495c0e242f12e80a
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-12-24 16:59:05 -0500

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
+import pkg_resources
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("pyproject.toml", "r") as fh:
+    pyproject = fh.read()
+
+version = (
+    [l.split(" = ")[1] for l in pyproject.split("\n") if "version = " in l][0]
+    .replace('"', "")
+    .replace("'", "")
+)
+
 setuptools.setup(
     name="blossom-wrapper",
-    version="0.4.0",
+    version=pkg_resources.get_distribution("blossom-wrapper").version,
     author="Grafeas Group",
     author_email="info@grafeas.org",
     description="Wrapper for the Blossom API",
